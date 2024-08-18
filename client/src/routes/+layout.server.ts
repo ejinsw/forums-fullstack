5b207a80ffd4c3efb259b9ec9f4a98ff82134585
Elijah Won <138832737+ejinsw@users.noreply.github.com>
2024-08-17 23:02:22 -0500

    Cookies issue

--- client/src/routes/+layout.server.ts
+++ client/src/routes/+layout.server.ts
@@ -5,7 +5,7 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ cookies, locals }) => {
 	const user: User | null = locals.user as User;
-	const jwt: string | undefined = cookies.get('jwt');
+	const jwt: string | undefined = cookies.get('jwt') || cookies.get('_vercel_jwt');
 
 	return {
 		user,
