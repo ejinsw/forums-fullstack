0e38923fe7f6691823653073ee7ccb953bb10a17
Elijah Won <138832737+ejinsw@users.noreply.github.com>
2024-10-08 11:52:05 -0700

    Update +page.server.ts

--- client/src/routes/+page.server.ts
+++ client/src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { env } from '$env/dynamic/public';
+// import { env } from '$env/dynamic/public';
 
 export const load: PageServerLoad = async (event) => {
 	// const res = await event.fetch(`${env?.PUBLIC_API_HOST}/api/posts`);
