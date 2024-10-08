6211648debfc94c51e4c3714c035e9bf7636897d
Elijah Won <138832737+ejinsw@users.noreply.github.com>
2024-10-08 11:50:57 -0700

    Update +page.server.ts

--- client/src/routes/+page.server.ts
+++ client/src/routes/+page.server.ts
@@ -3,15 +3,15 @@ import type { PageServerLoad } from './$types';
 import { env } from '$env/dynamic/public';
 
 export const load: PageServerLoad = async (event) => {
-	const res = await event.fetch(`${env?.PUBLIC_API_HOST}/api/posts`);
+	// const res = await event.fetch(`${env?.PUBLIC_API_HOST}/api/posts`);
 
-	if (!res.ok) {
-		throw error(500, 'Something went wrong');
-	}
+	// if (!res.ok) {
+	// 	throw error(500, 'Something went wrong');
+	// }
 
-    const posts = await res.json();
+    // const posts = await res.json();
 
-	return {
-		posts
-	};
+	// return {
+	// 	posts
+	// };
 };
